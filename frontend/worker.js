df6dd8688ee8595817f7d601896ccbf1245f08d3
northstarventuresca-livebridge <northstarventures.ca@gmail.com>
2026-09-21 17:14:21 -0600

    Route staging auth diagnostic page

--- frontend/worker.js
+++ frontend/worker.js
@@ -53,7 +53,7 @@ const PAGES = new Map([
   ["/offer", "/offer/index.html"],
   ["/offer/", "/offer/index.html"],
   ["/clerk-migrate", "/clerk-migrate/index.html"],
-  ["/clerk-migrate/", "/clerk-migrate/index.html"],
+  ["/clerk-migrate/", "/clerk-migrate/index.html"],\n  ["/auth-debug", "/auth-debug/index.html"],\n  ["/auth-debug/", "/auth-debug/index.html"],
 ]);
 
 function preserveQueryRedirect(
